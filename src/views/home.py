150f9e6760e12645f8bfbd7f5f96024df0e5c9ad
Paweł <limek008@gmail.com>
2026-02-16 11:49:24 +0100

    tescik bo na tel nie widac przycisku export import

--- src/views/home.py
+++ src/views/home.py
@@ -4,12 +4,12 @@ def HomeView(page):
     return ft.View(
         route="/",
         controls=[
-            ft.AppBar(title=ft.Text("Strona Główna"), bgcolor="blue"),
+            ft.AppBar(title=ft.Text("home"), bgcolor="blue"),
             ft.Container(
                 content=ft.Column([
                     ft.Text("pamietaj", size=30),
                     ft.FilledButton("PiPamietaj", on_click=lambda _: page.go("/pi")),
-                    ft.FilledButton("Birthdays", on_click=lambda _: page.go("/birthdays")),
+                    ft.FilledButton("Birthdays", on_click=lambda _: page.go("/eximport")),
                     ft.FilledButton("export/import", on_click=lambda _: page.go("/eximport")),
                 ]),
                 alignment=ft.Alignment(0, 0),
