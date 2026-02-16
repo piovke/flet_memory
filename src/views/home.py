5e3348ec7b49a2f3894333b805cc549f1ed5ced1
Paweł <limek008@gmail.com>
2026-02-16 18:28:47 +0100

    prettier

--- src/views/home.py
+++ src/views/home.py
@@ -4,14 +4,14 @@ def HomeView(page):
     return ft.View(
         route="/",
         controls=[
-            ft.AppBar(title=ft.Text("home"), bgcolor="blue"),
+            ft.AppBar(title=ft.Text("Pamietaj"), bgcolor="blue"),
             ft.Container(
                 content=ft.Column([
-                    ft.Text("pamietaj", size=30),
-                    ft.FilledButton("PiPamietaj", on_click=lambda _: page.go("/pi")),
-                    ft.FilledButton("PAO", on_click=lambda _: page.go("/pao")),
-                    ft.FilledButton("Dates", on_click=lambda _: page.go("/dates")),
-                    ft.FilledButton("export/import", on_click=lambda _: page.go("/eximport")),
+                    ft.Text("", size=30),
+                    ft.FilledButton("PiPamietaj", on_click=lambda _: page.go("/pi"), width=400),
+                    ft.FilledButton("PAO", on_click=lambda _: page.go("/pao"), width=400),
+                    ft.FilledButton("Dates", on_click=lambda _: page.go("/dates"), width=400),
+                    ft.FilledButton("export/import", on_click=lambda _: page.go("/eximport"), width=400),
                 ]),
                 alignment=ft.Alignment(0, 0),
                 expand=True
